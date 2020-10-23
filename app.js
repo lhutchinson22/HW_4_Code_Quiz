e24568b4fa5c753edc796a39a50c91d5edb08f61
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-23 08:58:39 -0700

    re did timer and working on displaying text onto html

--- app.js
+++ app.js
@@ -1,86 +1,130 @@
 console.log("sanity check");
 
-// timer header
-let timerCountdown = document.querySelector("#timer");
+var timerCount = 100;
+var timerEl = document.querySelector("#timer");
 
-// timer function
-let count = 10;
+timerEl.textContent = timerCount;
 
-function startTimerCoundown() {
-  var timer = setInterval(function () {
-    // displays timer into the html
-    document.getElementById("timer").innerHTML = count;
-    count--;
-    console.log(count);
-    if (count === 0) {
-      console.log("STOOOOOPPED");
-      timerCountdown.textContent = "YOU LOSE.";
-      clearInterval(timer);
-    }
-  }, 1000);
-}
+var timer = window.setInterval(function () {
+  timerCount--;
+  timerEl.textContent = timerCount;
 
-startTimerCoundown();
+  if (timerCount === 0) {
+    clearInterval(timer);
+  }
+}, 1000);
 
-// quiz questions
-var myQuestions = [
-  {
-    question: "Commonly used data types do not include",
-    answers: {
-      a: "strings",
-      b: "booleans",
-      c: "alerts",
-      d: "numbers",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "The condition of an IF / ELSE statement is closed with___",
-    answers: {
-      a: "quotes",
-      b: "curly brackets",
-      c: "parenthesis",
-      d: "square function",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Arrays in javascript can be used to store",
-    answers: {
-      a: "numbers and strings",
-      b: "other arrays",
-      c: "booleans",
-      d: "all of the above",
-    },
-    correctAnswer: "d",
-  },
-  {
-    question:
-      "String values must be enclosed with___ when being assigned to variables",
-    answers: {
-      a: "commas",
-      b: "curly brackets",
-      c: "quotes",
-      d: "parentheses",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question:
-      "A very useful tool used during development and debugging for printing content to the debugger is",
-    answers: {
-      a: "Javascript",
-      b: "terminal / bash",
-      c: "for loops",
-      d: "console.log",
-    },
-    correctAnswer: "d",
-  },
-];
-
-//using jquery for the submit button to run a function
 $(document).ready(function () {
-  $("#submit").on("click", function () {
-    console.log("You clicked a button!!");
+  //using jquery for buttons when pressed
+  //   $("li").on("click", function () {
+  //     console.log("You clicked a button!!");
+
+  var questions = [
+    {
+      q: "Commonly used data types do not include",
+      answers: {
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+      },
+      correctAnswer: "c",
+    },
+    {
+      q: "The condition of an IF / ELSE statement is closed with___",
+      answers: {
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square function",
+      },
+      correctAnswer: "c",
+    },
+    {
+      q: "Arrays in javascript can be used to store",
+      answers: {
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above",
+      },
+      correctAnswer: "d",
+    },
+    {
+      q:
+        "String values must be enclosed with___ when being assigned to variables",
+      answers: {
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parentheses",
+      },
+      correctAnswer: "c",
+    },
+    {
+      q:
+        "A very useful tool used during development and debugging for printing content to the debugger is",
+      answers: {
+        a: "Javascript",
+        b: "terminal / bash",
+        c: "for loops",
+        d: "console.log",
+      },
+      correctAnswer: "d",
+    },
+  ];
+
+  var questionsPointer = 0;
+
+  var question = document.querySelector("#question");
+  var optionA = document.querySelector("#optionA");
+  var optionB = document.querySelector("#optionB");
+  var optionC = document.querySelector("#optionC");
+
+  function setQuestions() {
+    if (questionsPointer === questions.length) {
+      clearInterval(timer);
+      alert("You are done with " + timerCount + " time left");
+      return;
+    }
+
+    question.textContent = questions[questionsPointer].q;
+    optionA.textContent = questions[questionsPointer].answers[0];
+    optionB.textContent = questions[questionsPointer].answers[1];
+    optionC.textContent = questions[questionsPointer].answers[2];
+  }
+  setQuestions();
+
+  optionA.addEventListener("click", function () {
+    if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
+      console.log("This is correct");
+      questionsPointer++;
+      setQuestions();
+    } else {
+      console.log("wrong");
+      timerCount -= 10;
+    }
+  });
+
+  optionB.addEventListener("click", function () {
+    if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
+      console.log("This is correct");
+      questionsPointer++;
+      setQuestions();
+    } else {
+      console.log("wrong");
+      timerCount -= 10;
+    }
+  });
+
+  optionC.addEventListener("click", function () {
+    if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
+      console.log("This is correct");
+      questionsPointer++;
+      setQuestions();
+    } else {
+      console.log("wrong");
+      timerCount -= 10;
+    }
   });
 });
